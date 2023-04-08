74bd05c99734d2a51b5e8cb84f842353ae4d3f86
bdcocofye <130224544+bdcocofye@users.noreply.github.com>
2023-04-08 04:56:25 -0700

    Add files via upload

---
+++ zazure_mod_src_load.lua
@@ -0,0 +1,8 @@
+bdcoco on top niga boy
+
+--This is the SOURCE code, NOT obfuscated
+--Beautified source, super easy to understand
+--Consider joining original azure discord!
+
+--Azure Modded Loadstring:
+loadstring(game:HttpGet("https://pastebin.com/raw/SH5Pyji6"))()
