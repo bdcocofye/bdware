a08f10ff28be7c7c97c57866f78f7307a43d9378
bdcocofye <130224544+bdcocofye@users.noreply.github.com>
2023-04-08 05:05:32 -0700

    Update and rename zazure_mod_src_load.lua to bdwarecoco

--- zazure_mod_src_load.lua
+++
@@ -1,8 +0,0 @@
-bdcoco on top niga boy
-
---This is the SOURCE code, NOT obfuscated
---Beautified source, super easy to understand
---Consider joining original azure discord!
-
---Azure Modded Loadstring:
-loadstring(game:HttpGet("https://pastebin.com/raw/SH5Pyji6"))()
